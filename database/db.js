db96e5dd2401d0f717da2e11b18eab6b0b5ca8dd
kirankumarn07 <kirankumar.naga7@gmail.com>
2024-02-07 23:45:44 +0530

    Gmail clone commit

---
+++ database/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const USERNAME = process.env.DB_USERNAME;
+// const PASSWORD = process.env.DB_PASSWORD; 
+
+const Connection = () => {
+    const DB_URI = `mongodb+srv://kirankumarnaga7:Nkiran07@cluster0.ata8crg.mongodb.net/?retryWrites=true&w=majority`;
+    try {
+        mongoose.connect(DB_URI, { useNewUrlParser: true });
+        mongoose.set('strictQuery', false);
+        console.log('Database connected sucessfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message)
+    }
+}
+
+export default Connection;
